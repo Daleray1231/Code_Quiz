c3f65fcb2c89fdc6c15f24c60abc468002a0f57f
Daleray1231 <daleray1231@gmail.com>
2023-09-07 11:34:21 -0500

    added features

--- java/script.js
+++ java/script.js
@@ -1,15 +1,23 @@
-var timeLeft = 75;
+var timeLeft = 30;
 var elem = document.getElementById('timer');
+var scoreElement = document.querySelector("#score");
+var yourScore = 0;
 var timerId;
 var index = 0;
+var containerElement = document.querySelector("#container");
 var questionElement = document.querySelector("#questions");
 var responseElement = document.querySelector("#responses");
-
+var initialsEl = document.querySelector('#initials');
 var startButton = document.querySelector('#startButton');
 var answerList = document.querySelector('#responses');
 var title = document.querySelector("#title");
+var isQuizOver = false;
+
+var submit = document.querySelector('#submit');
+
 
 startButton.addEventListener('click', function () {
+    title.style.display = 'none';
     startButton.style.display = 'none';
     answerList.style.display = '';
     startCountdown();
@@ -22,11 +30,12 @@ answerList.addEventListener('click', function (event) {
     var correctAnswerIndex = questions[index].answer;
 
     if (selectedAnswer === questions[index].responses[correctAnswerIndex]) {
+        yourScore = yourScore + 25;
     } else {
         timeLeft -= 10;
     }
 
-    navigate(1); 
+    navigate(1);
     if (index < questions.length) {
         renderQuestion();
         renderAnswers();
@@ -41,7 +50,7 @@ function startCountdown() {
 }
 
 function countdown() {
-    if (timeLeft <= 0) {
+    if (timeLeft <= 0 || isQuizOver) {
         clearInterval(timerId);
         endQuiz();
     } else {
@@ -51,17 +60,17 @@ function countdown() {
 }
 
 var questions = [
-    { question: "What is a function?", responses: [ "Reusable code", "Primitive value", "None of the above" ], answer: 0 },
-    { question: "What is an array?", responses: [ "List of values", "Key value pairs", "None of the above" ], answer: 2 },
-    { question: "What is a primitive value?", responses: [ "123", "1234", "None of the above" ], answer: 2 },
-    { question: "What is the abbreviation JSON?", responses: [ "JASON", "Javascript notation object", "None of the above" ], answer: 1 },
+    { question: "What is a function?", responses: ["Reusable code", "Primitive value", "None of the above"], answer: 0 },
+    { question: "What is an array?", responses: ["List of values", "Key value pairs", "None of the above"], answer: 2 },
+    { question: "What is a primitive value?", responses: ["123", "1234", "None of the above"], answer: 2 },
+    { question: "What is the abbreviation JSON?", responses: ["JASON", "Javascript notation object", "None of the above"], answer: 1 },
 ];
 
 function navigate(direction) {
     index = index + direction;
     if (index < 0) {
         index = questions.length - 1;
-    } else if (index >= questions.length) {}
+    } else if (index >= questions.length) { }
 }
 
 function renderQuestion() {
@@ -69,18 +78,35 @@ function renderQuestion() {
 }
 
 function renderAnswers() {
-    answerList.innerHTML = ""; 
+    answerList.innerHTML = "";
     for (var i = 0; i < questions[index].responses.length; i++) {
         var li = document.createElement("li");
         li.textContent = questions[index].responses[i];
         answerList.appendChild(li);
-        // answerList.style.display= "block";
     }
 }
 
 function endQuiz() {
-    alert("Quiz ended. Your score: " + timeLeft);
+    questionElement.style.display = 'none';
+    answerList.style.display = 'none';
+    isQuizOver = true;
+    scoreElement.textContent = yourScore;
+
 }
 
-// renderQuestion();
-// renderAnswers();
+submit.addEventListener("click", saveScore);
+
+function saveScore() {
+    var initials = initialsEl.value.trim();
+
+    localStorage.setItem("initials", initials);
+    localStorage.setItem("score", yourScore);
+
+    if (!localStorage.getItem("highscores")) {
+        localStorage.setItem("highscores", JSON.stringify([{ initial: initials, score: yourScore }]));
+    } else {
+        var highscores = JSON.parse(localStorage.getItem("highscores"));
+        highscores.push([{ initial: initials, score: yourScore }]);
+        localStorage.setItem("highscores", JSON.stringify(highscores));
+    }
+}
